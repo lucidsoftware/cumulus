c5216f4149fd33176bb6f0f5d0927a6020a7b187
Mark Siebert <mark@lucidchart.com>
2016-06-28 10:12:25 -0600

    Create a separate sqs spec rake task

--- rakefile.rb
+++ rakefile.rb
@@ -3,7 +3,11 @@ require 'rspec/core/rake_task'
 
 task :default => :spec
 
-RSpec::Core::RakeTask.new(:spec) do |task|
+task :spec do
+  Rake::Task['spec_sqs'].execute
+end
+
+RSpec::Core::RakeTask.new(:spec_sqs) do |task|
   task.rspec_opts = ['-r ./spec/rspec_config.rb']
   task.pattern = 'spec/sqs/*_spec.rb'
 end
